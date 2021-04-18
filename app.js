9dbed7db8b5717568026f4ce83db359e7396a6a8
Ameeno09 <noorul@tmmt.com>
2021-04-18 19:37:53 +0530

    code updated

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-var express = require("express");
-var path = require("path");
-var cors = require("cors");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var indexRouter = require("./routes");
-var urlRouter = require("./routes/url");
-const { route } = require("./routes");
-const { MongoClient, url, dbname, ObjectId } = require("./config");
+var indexRouter = require('./routes');
+const {MongoClient, url, dbname, ObjectId} = require('./config');
 
 var app = express();
-var router = express.Router();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/api", indexRouter);
-app.use("/", urlRouter);
+app.use('/api', indexRouter);
+app.get('/', (req, res) => {
+  res.send('You are viewing URL Shortner backend');
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ var cors = require('cors')
 var app = express.Router();
 var usersRouter = require("./users");
 var urlShortRouter = require("./urlshort");
+var urlRouter = require("./url");
 
 app.use("/users", usersRouter);
 app.use("/urlShort", urlShortRouter);
+app.use("/url", urlRouter);
 
 module.exports = app;
